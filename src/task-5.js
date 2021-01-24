86f2607cb8121c7601f39fde4f5991d3e0512a1c
Oksana Zherevko <squirrelksi@ukr.net>
2021-01-24 16:49:30 +0200

    Добавляет файлы

---
+++ src/task-5.js
@@ -0,0 +1,20 @@
+function checkForSpam (str) { 
+  'use strict';
+  // Write code under this line
+  const newStr = str.toLowerCase();
+  const spamWord = newStr.includes("sale") || newStr.includes("spam");
+  return spamWord;
+}
+
+//console.log(checkForSpam('Latest technology news')); // false
+
+//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+//console.log(checkForSpam('Get best sale offers now!')); // true
+
+//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+
+//Функция предикат возвращает true или false
+
+//Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку. Функция проверяет ее на содержание слов spam и sale. Если нашли запрещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false. Слова в строке могут быть в произвольном регистре.
